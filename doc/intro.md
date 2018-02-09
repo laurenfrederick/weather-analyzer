26b9f7a69c2a1f6c4c7d32eccce56e2bf26deb76
Lauren Frederick <lpfreder@mbp-lpfreder.edn.ecs.nasa.gov>
2018-02-09 15:56:56 -0500

    Framework

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to weather-analyzer
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/weather_analyzer/core_test.clj
@@ -0,0 +1,7 @@
+(ns weather-analyzer.core-test
+  (:require [clojure.test :refer :all]
+            [weather-analyzer.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
